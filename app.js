87b7a9932cbcbcd3ef8d6be920abde33af96d089
Benja Brane <benjabrane76@gmail.com>
2023-09-18 21:57:05 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,175 @@
+const edad = prompt("Por favor, ingresa tu edad:");
+
+        // Verificar la edad
+        if (edad === null) {
+            // Si el usuario cancela el prompt
+            alert("Debes ingresar tu edad para acceder a esta página.");
+        } else if (parseInt(edad) < 18) {
+            // Si la edad es menor de 18
+            alert("Lo siento, debes tener al menos 18 años para acceder a esta página.");
+        } else {
+            // Si la edad es válida, continuar con la página
+            alert("Bienvenido a nuestra página.");
+        }
+
+const entrada = prompt("Por favor, ingresa algo:");
+console.log("Capturaste la entrada: " + entrada);
+
+let entradaUsuario;
+
+
+entradaUsuario = prompt("Por favor, ingresa tu nombre:");
+
+
+const miObjeto = {
+
+nombreUsuario: entradaUsuario,
+
+
+saludar: function() {
+}
+};
+
+
+miObjeto.saludar();
+
+function dividir(dividendo, divisor) {
+    
+if (divisor === 0) {
+return "No es posible dividir por cero";
+    }
+
+    return dividendo / divisor;
+}
+
+
+const resultado = dividir(10, 2); 
+console.log("Resultado: " + resultado);
+
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
+
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
+	}
+});
+
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
+
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
+
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+		rowProduct.append(containerProduct);
+
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+};
